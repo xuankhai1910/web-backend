64931578ed5e783a13461c010c8ffeaeac57015a
Xuan Khai <xuankhai19102004@gmail.com>
2026-07-11 15:36:33 +0700

    fix update user

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,10 +1,6 @@
-import { OmitType } from '@nestjs/mapped-types';
+import { OmitType, PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import { IsNotEmpty } from 'class-validator';
 
-export class UpdateUserDto extends OmitType(CreateUserDto, [
-  'password',
-] as const) {
-  // @IsNotEmpty({ message: 'Id không được để trống' })
-  // _id: string;
-}
+export class UpdateUserDto extends PartialType(
+  OmitType(CreateUserDto, ['password'] as const),
+) {}
